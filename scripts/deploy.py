f14dcbf99997e71ecc4e4f3e5cb60920f803bd2c
Rad1m <radim.simanek@protonmail.ch>
2021-11-25 12:53:57 +0000

    FundMe working

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,13 @@
-from brownie import FundMe, network, config, MockV3Aggregator
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import (
     get_account,
     deploy_mocks,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
 )
 
-# from web3 import Web3 # not needed anymore as it's already in helpful_scripts.py
-
 
 def deploy_fund_me():
     account = get_account()
-
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
@@ -24,7 +21,8 @@ def deploy_fund_me():
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
-    print("Contract deployed to {fund_me.address}")
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
