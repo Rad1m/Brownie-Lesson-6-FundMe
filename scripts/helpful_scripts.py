56cd172c67461453f87caa6527df848b725c59fa
Rad1m <radim.simanek@protonmail.ch>
2021-11-26 13:13:00 +0000

    Alchemy test net added

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,9 +1,26 @@
 from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
+
 ##########################################################################################
 #### IF THE CONTRACT IS NOT RUNNING, DELETE GANACHE 1332 FOLDER IN /BUILD/DEPLOYMENTS ####
 ##########################################################################################
+
+##################################################
+### THIS IS TO PRINT COLORFUL TEXT IN TERMINAL ###
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+############## END OF COLORS ####################
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
